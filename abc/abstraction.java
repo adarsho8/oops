3890973f800faa25dc88ddb99acf087cea6fca21
Shreyash Mungilwar <shreyashmungilwar@gmail.com>
2023-04-15 13:55:25 +0530

    first commit

---
+++ abc/abstraction.java
@@ -0,0 +1,8 @@
+package abc;
+class abstraction{
+   public static void main(String[] args){
+      System.out.println("hi main");
+      inheritance.main();
+
+   }
+}
---
+++ abc/inheritance.java
@@ -0,0 +1,17 @@
+package abc;
+class One{
+    void print(){
+        System.out.println("how are you");
+    }
+}
+class two extends One{
+    void print1(){
+        System.out.println("good");
+    }
+}
+public class inheritance {
+    public static void main(String[] args)
+    {
+
+    }
+}
